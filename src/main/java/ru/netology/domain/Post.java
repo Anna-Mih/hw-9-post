99c96363d3d902f901d360d9038be03b6c8966ae
anna-mih <anedbalskaya@yandex.ru>
2022-04-21 11:58:33 +0300

    initial commit

---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,11 @@
+package ru.netology.domain;
+
+public class Post {
+    public String name;
+    public String passport;
+    public String patronimic;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+    public FormDate birthday;
+}
